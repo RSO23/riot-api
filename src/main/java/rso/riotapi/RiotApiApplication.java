ca6036e6eec5c866a86b0ca014aefc4466ee8b33
Blarc <jakob.malezic@gmail.com>
2020-12-03 19:21:15 +0100

    added consul discovery and cleaned up poms

--- src/main/java/rso/riotapi/RiotApiApplication.java
+++ src/main/java/rso/riotapi/RiotApiApplication.java
@@ -2,11 +2,13 @@ package rso.riotapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RiotApiApplication {
 
 	@Bean
