1dfc7d1d615b6929fb25874d7297cc6519873995
Blarc <jakob.malezic@gmail.com>
2020-12-05 16:29:45 +0100

    fixed github workflow

--- src/test/java/rso/riotapi/RiotApiApplicationTests.java
+++ src/test/java/rso/riotapi/RiotApiApplicationTests.java
@@ -3,7 +3,7 @@ package rso.riotapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest({"spring.cloud.consul.enabled=false"})
 class RiotApiApplicationTests {
 
 	@Test
