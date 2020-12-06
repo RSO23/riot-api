7eed64a221b90febd0f68853a378462a0fedc558
Blarc <jakob.malezic@gmail.com>
2020-12-06 19:02:17 +0100

    added centralized logging with logit.io

--- src/main/java/rso/riotapi/RiotApiApplication.java
+++ src/main/java/rso/riotapi/RiotApiApplication.java
@@ -1,10 +1,14 @@
 package rso.riotapi;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,6 +26,12 @@ public class RiotApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RiotApiApplication.class, args);
+	}
+
+	@EventListener(ContextRefreshedEvent.class)
+	public void contextRefreshEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		ApplicationContext applicationContext = contextRefreshedEvent.getApplicationContext();
+		MDC.put("applicationName", applicationContext.getId());
 	}
 
 }
