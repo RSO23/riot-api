50fe1f5f6e19bb73538d73c15df9405495fd4af2
Blarc <jakob.malezic@gmail.com>
2020-12-09 10:21:55 +0100

    added @EnableDiscoveryClient

--- src/main/java/rso/riotapi/RiotApiApplication.java
+++ src/main/java/rso/riotapi/RiotApiApplication.java
@@ -7,6 +7,7 @@ import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +28,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 @EnableSwagger2
 public class RiotApiApplication {
 
